8bccb5f77967a72207faef294fe7e830e93b7915
skosta <scott.kosta@yahoo.com>
2024-08-29 17:10:01 -0400

    change to Https

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -6,7 +6,7 @@ const packageJson = require('../package.json');
 const devConfig = {
   mode: 'development',
   output: {
-    publicPath: 'http://localhost:8080/',
+    publicPath: 'https://localhost:8080/',
   },
   devServer: {
     port: 8080,
@@ -18,9 +18,9 @@ const devConfig = {
     new ModuleFederationPlugin({
       name: 'container',
       remotes: {
-        marketing: 'marketing@http://localhost:8081/remoteEntry.js',
-        auth: 'auth@http://localhost:8082/remoteEntry.js',
-        dashboard: 'dashboard@http://localhost:8083/remoteEntry.js',
+        marketing: 'marketing@https://localhost:8081/remoteEntry.js',
+        auth: 'auth@https://localhost:8082/remoteEntry.js',
+        dashboard: 'dashboard@https://localhost:8083/remoteEntry.js',
       },
       shared: packageJson.dependencies,
     }),
