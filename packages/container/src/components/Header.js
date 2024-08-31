44461d61e1341b7f718f29dfd2a0b4aca71c90c2
skosta <scott.kosta@yahoo.com>
2024-08-31 10:12:03 -0400

    changinh header

--- packages/container/src/components/Header.js
+++ packages/container/src/components/Header.js
@@ -79,7 +79,7 @@ export default function Header({ isSignedIn, onSignOut }) {
             component={RouterLink}
             to="/"
           >
-            App
+            Store Front
           </Typography>
           <Button
             color="primary"
